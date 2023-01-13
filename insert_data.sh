2c2cddbf8f2322d029890798abbe95eeed616927
ptrszmczk <ptrszmczk@gmail.com>
2023-01-13 17:21:45 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,28 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE teams RESTART IDENTITY CASCADE")
+
+cat games.csv | while IFS="," read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    TEAM_WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+    TEAM_OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+
+    if [[ -z $TEAM_WINNER_ID ]]
+    then
+      INSERT_TEAM_RESULT_WINNER=$($PSQL "insert into teams(name) values('$WINNER')")
+    fi
+
+    if [[ -z $TEAM_OPPONENT_ID ]]
+    then
+      INSERT_TEAM_RESULT_OPPONENT=$($PSQL "insert into teams(name) values('$OPPONENT')")
+    fi
+
+    GET_WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+    GET_OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+
+    INSERT_GAME_RESULT=$($PSQL "insert into games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values($YEAR, '$ROUND', $GET_WINNER_ID, $GET_OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  fi
+done
